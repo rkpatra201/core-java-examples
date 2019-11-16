9b66f010ef9841725ca29f545eb1d6c0f436e9b9
RabindraPatra <rabindrapatra825@gmail.com>
2019-11-16 21:19:05 +0530

    segregate 0 and 1

---
+++ java-ds/src/main/java/com/java/main/Segregate01.java
@@ -0,0 +1,32 @@
+package com.java.main;
+
+import java.util.Arrays;
+
+public class Segregate01 {
+    public static void main(String[] args) {
+        int[] arr={1,0,1,0};
+        int count1=0;
+        int count0=0;
+        for(int i:arr)
+        {
+            if(i==1)
+            {
+                count1++;
+            }
+            if(i==0)
+            {
+                count0++;
+            }
+        }
+
+        for(int i=0;i<count1;i++)
+        {
+            arr[i]=1;
+        }
+        for(int i=count1;i<count0+count1;i++)
+        {
+            arr[i]=0;
+        }
+        System.out.println(Arrays.toString(arr));
+    }
+}
