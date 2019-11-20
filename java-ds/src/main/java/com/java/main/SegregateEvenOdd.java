3e3e7d26dd1794e8a1176211fd2f80d770273ae9
rkpatra201 <rabindrapatra825@gmail.com>
2019-11-20 21:20:25 +0530

    update for even and odd

--- java-ds/src/main/java/com/java/main/SegregateEvenOdd.java
+++ java-ds/src/main/java/com/java/main/SegregateEvenOdd.java
@@ -9,11 +9,10 @@ public class SegregateEvenOdd {
 
     public static void quickEvenOdd() {
         int arr[] = {1, 5, 4, 2, 3, 7, 9, 10, 10};
-        int pivot = arr[arr.length - 1];
         int i = -1;
-        int j = 1 + 1;
-        int pivotRem = pivot % 2;
-        while (j < arr.length - 1) {
+        int j = 0;
+        int pivotRem = 0; // 0 or 1 for even or odd starter
+        while (j < arr.length) {
             if (arr[j] % 2 == pivotRem) { // == or !=
                 i++;
                 int t = arr[i];
@@ -21,11 +20,6 @@ public class SegregateEvenOdd {
                 arr[j] = t;
             }
             j++;
-        }
-        if (arr[i] % 2 == pivotRem) {
-            int t = arr[i + 1];
-            arr[i + 1] = arr[arr.length - 1];
-            arr[arr.length - 1] = t;
         }
         System.out.println(Arrays.toString(arr));
     }
