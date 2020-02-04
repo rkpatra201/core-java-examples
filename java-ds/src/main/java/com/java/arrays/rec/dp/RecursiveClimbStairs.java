e7e58c77722d0d3901142d9cf565b8b3c541c848
rkpatra201 <rabindrapatra825@gmail.com>
2020-02-04 20:57:21 +0530

    refactor base case for climbing stairs

--- java-ds/src/main/java/com/java/arrays/rec/dp/RecursiveClimbStairs.java
+++ java-ds/src/main/java/com/java/arrays/rec/dp/RecursiveClimbStairs.java
@@ -3,15 +3,15 @@ package com.java.arrays.rec.dp;
 public class RecursiveClimbStairs {
     public static void main(String[] args) {
 
-        System.out.println(climbStairs(4));
+        System.out.println(climbStairs(6));
     }
 
     public static int climbStairs(int n) {
-        if (n == 0 || n == 1) {
+        if (n == 0 ) { // base case
             return 1;
         }
-        else if (n == 2) {
-            return 2;
+        else if (n < 0) { // base case
+            return 0;
         }
         return climbStairs(n - 3) + climbStairs(n - 2) + climbStairs(n - 1);
     }
