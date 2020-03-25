a7902abb04d5cf4ae267e28b6e365f7c5a36c53b
rkpatra201 <rabindrapatra825@gmail.com>
2020-03-25 23:18:00 +0530

    substr with no repeating char and median of 2 arrays

--- java-ds/src/main/java/com/java/string/substr/SubStrWithNoRepeatingCharacters.java
+++ java-ds/src/main/java/com/java/string/substr/SubStrWithNoRepeatingCharacters.java
@@ -5,12 +5,20 @@ import java.util.Arrays;
 public class SubStrWithNoRepeatingCharacters {
     public static void main(String[] args) {
 
-        String input = "amanda"; // mand = 4
-        input = "GEEKSFORGEEKS"; // EKSFORG = 7
-        input = "sachin"; // sachin = 6
-        input = "ABCA"; // ABC = 3
-        input = "ABCAD"; // BCAD = 4
-        input = "AMAANDA";
+        String input = "dadbc"; // mand = 4
+//        input = "GEEKSFORGEEKS"; // EKSFORG = 7
+//        input = "sachin"; // sachin = 6
+//        input = "ABCA"; // ABC = 3
+//        input = "ABCAD"; // BCAD = 4
+//        input = "AMAANDA";
+        int res = substrWithNrc(input);
+    }
+    private static int substrWithNrc(String input)
+    {
+        if(input.length() == 0)
+        {
+            return 0;
+        }
         int currentLength = 1;
         int maxLength = 1;
         int charLastIndex;
@@ -19,8 +27,8 @@ public class SubStrWithNoRepeatingCharacters {
         Arrays.fill(visitedCharsIndex, -1);
         visitedCharsIndex[input.charAt(0)] = 0;
 
-        int begin = 0;
-        int end = 0;
+//        int begin = 0;
+//        int end = 0;
         for (currentIterationIndex = 1; currentIterationIndex < input.length(); currentIterationIndex++) {
             charLastIndex = visitedCharsIndex[input.charAt(currentIterationIndex)];
 
@@ -28,8 +36,8 @@ public class SubStrWithNoRepeatingCharacters {
                 currentLength++;
             } else {
                 if (maxLength < currentLength) {
-                    begin = currentIterationIndex - currentLength; // adjust begin to post the previous last char position
-                    end = currentIterationIndex;
+            //        begin = currentIterationIndex - currentLength; // adjust begin to post the previous last char position
+              //      end = currentIterationIndex;
 
                     maxLength = currentLength;
                     currentLength = currentIterationIndex - charLastIndex;// count length after the charLastIndex
@@ -40,13 +48,14 @@ public class SubStrWithNoRepeatingCharacters {
             visitedCharsIndex[input.charAt(currentIterationIndex)] = currentIterationIndex;
         }
         if (maxLength < currentLength) { // to handle if all are unique chars or a larger substring at the end only.
-            begin = currentIterationIndex - currentLength; // adjust begin to post the previous last char position
-            end = currentIterationIndex;
+        //    begin = currentIterationIndex - currentLength; // adjust begin to post the previous last char position
+          //  end = currentIterationIndex;
 
             maxLength = currentLength;
         }
-        System.out.println("longest substring between: " + begin + " & " + end);
-        System.out.println(Arrays.copyOfRange(input.toCharArray(), begin, end));
+      //  System.out.println("longest substring between: " + begin + " & " + end);
+      //  System.out.println(Arrays.copyOfRange(input.toCharArray(), begin, end));
         System.out.println(maxLength);
+        return maxLength;
     }
 }
