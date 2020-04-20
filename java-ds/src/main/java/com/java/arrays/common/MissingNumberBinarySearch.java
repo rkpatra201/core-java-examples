ee71889874e975146bdd714b0f75cc54aa0d547a
rkpatra201 <rabindrapatra825@gmail.com>
2020-04-20 21:11:12 +0530

    TKT-200420: missing number in 1-n array binary search

--- java-ds/src/main/java/com/java/arrays/common/MissingNumberBinarySearch.java
+++ java-ds/src/main/java/com/java/arrays/common/MissingNumberBinarySearch.java
@@ -1,27 +1,65 @@
 package com.java.arrays.common;
 
+
+// always find the incorrect place.
+
+/**
+ * incorrect place means an element not placed at its
+ * index.
+ * so check incorrect place at left.
+ * when left is incorrect all its right or mid is incorrect.
+ * so right = mid
+ * <p>
+ * check incorrect place at middle.
+ * when middle is at incorrect place, all its right is correct.
+ * so right=mid.
+ * <p>
+ * check incorrect place at right.
+ * when right is at incorrect place, all its left are incorrect.
+ * so left = mid
+ * <p>
+ * so this ordering check matters
+ * left mid right
+ */
 public class MissingNumberBinarySearch {
     public static void main(String[] args) {
-        int arr[] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        //{1, 3, 4, 5}
+        //{1,3}
+
+        int arr[] = {1, 2, 3, 4, 5, 7, 8, 9, 10};
 
         int left = 0;
-        int right = arr.length;
+        int right = arr.length - 1;
 
         int missingValue = binarySearch(left, right, arr);
         System.out.println(missingValue);
     }
 
-    private static int binarySearch(int left, int right, int[] a) {
-        int middle = left + (right - left) / 2;
-        if (left >= right) {
-            if (middle >= a.length) {
-                return -1;
+    public static int binarySearch(int left, int right, int[] arr) {
+        int mid = 0;
+        while (left < right) {
+            mid = (left + right) / 2;
+
+            if (right - left == 1) {
+                break;
             }
-            return a[middle] - 1;
+            if (arr[left] - left > 1) {
+                right = mid;
+            } else if (arr[mid] - mid > 1) {
+                right = mid;
+            } else if (arr[right] - right > 1) {
+                left = mid;
+            }
+
+            if (left == 0 && right == arr.length - 1) {
+                break;
+            }
         }
-        if (a[0] + middle < a[middle])
-            return binarySearch(left, middle, a);
-        else
-            return binarySearch(middle + 1, right, a);
+
+        if (right - left == 1) {
+            return (arr[right] + arr[left]) / 2;
+        }
+        return -1;
     }
+
 }
